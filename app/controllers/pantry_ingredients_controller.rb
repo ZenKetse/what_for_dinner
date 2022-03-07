608c6f915588a3d8a133e331812df403bb811253
afonso miguel <afonso.miguel1995@gmail.com>
2022-03-07 13:13:08 +0000

    user can update pantry after cooking

--- app/controllers/pantry_ingredients_controller.rb
+++ app/controllers/pantry_ingredients_controller.rb
@@ -1,14 +1,12 @@
 class PantryIngredientsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-
     params[:ingredients].each do |ingredient|
       @ingredient = Ingredient.find(ingredient)
       @pantry_ingredient = PantryIngredient.new(ingredient_id: ingredient, in_stock: true, default: true)
       @pantry_ingredient.pantry = current_user.pantry
       @pantry_ingredient.save!
     end
-
 
     respond_to do |format|
       format.html
@@ -17,7 +15,8 @@ class PantryIngredientsController < ApplicationController
   end
 
   def update
-    @pantry = current_user.pantry
+    @pantry_ingredient = PantryIngredient.find(params[:id])
+    @pantry_ingredient.update!(in_stock: false)
   end
 
   def destroy
--- app/models/pantry_ingredient.rb
+++ app/models/pantry_ingredient.rb
@@ -4,7 +4,6 @@ class PantryIngredient < ApplicationRecord
   belongs_to :pantry
   belongs_to :ingredient
   has_many :users, through: :pantry
-  validates :pantry_id, :ingredient_id, :in_stock, :default, presence: true
 
   pg_search_scope :search_by_name,
                   against: :name,
