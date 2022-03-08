bef3223a9f3a9cc3613827ded1888641fd700db9
afonso miguel <afonso.miguel1995@gmail.com>
2022-03-08 17:45:39 +0000

    finished shopping list page

--- app/controllers/pantry_ingredients_controller.rb
+++ app/controllers/pantry_ingredients_controller.rb
@@ -15,13 +15,17 @@ class PantryIngredientsController < ApplicationController
   end
 
   def update
-    if params[:pantry_ingredient].present?
-      @pantry_ingredient = PantryIngredient.find(params[:pantry_ingredient][:id])
+    if params[:pantry_ingredient][:id].class == Array
+      params[:pantry_ingredient][:id].each do |pantry_ingredient_id|
+        @pantry_ingredient = PantryIngredient.find(pantry_ingredient_id)
+        @pantry_ingredient.default = true
+        @pantry_ingredient.update(in_stock: true)
+      end
     else
-      @pantry_ingredient = PantryIngredient.find(params[:id])
+      @pantry_ingredient = PantryIngredient.find(params[:pantry_ingredient][:id])
+      @pantry_ingredient.default = true
+      @pantry_ingredient.update(in_stock: false)
     end
-    @pantry_ingredient.default = true
-    @pantry_ingredient.update(in_stock: false)
   end
 
   def destroy
--- app/javascript/controllers/shopping_to_pantry_controller.js
+++ app/javascript/controllers/shopping_to_pantry_controller.js
@@ -1,20 +1,25 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ 'ingredientCard' ]
+  static targets = [ 'message' ]
   static values = { id: Number, url: String }
+
+  connect() {
+    console.log(this.idValue);
+    console.log(this.urlValue);
+  }
 
   send_everything() {
     let url = this.urlValue;
-    let ingredients = [];
-    this.ingredientCardTargets.forEach((ingredient) => ingredients.push(ingredient.dataset.ingredientCardIdValue))
-    console.log(url);
+    let ingredients = []
+    document.querySelectorAll(".card-ingredient").forEach((ingredient) => ingredients.push(ingredient.dataset.shoppingListIdValue));
     fetch(url, {
       method: "PATCH",
       headers: {  "Accept": "text/plain", 'Content-Type': 'application/json' } ,
       body: JSON.stringify({ id: ingredients })
     })
-    this.ingredientCardTargets.remove();
+    document.querySelectorAll(".card-ingredient").forEach((ingredient) => ingredient.remove());
+    document.querySelectorAll("p").forEach((p) => p.remove())
   }
 
 }
