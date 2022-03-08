dde2f0554bec81c23b19849bd2c0d2612110b9e8
afonso miguel <afonso.miguel1995@gmail.com>
2022-03-08 10:27:42 +0000

    pantry cards working, fixed some shopping list bugs, javascript responsive

--- app/javascript/controllers/send_ingredients_controller.js
+++ app/javascript/controllers/send_ingredients_controller.js
@@ -4,14 +4,11 @@ export default class extends Controller {
   static targets = [ "list", "ingredientCard" ]
   static values = { id: Number, url: String }
 
-  connect() {
-    console.log(this.urlValue);
-  }
-
   send() {
     let pantry_ingredients = []
     let ingredients_list = document.querySelectorAll(".clicked-background").forEach((ingredient) => pantry_ingredients.push(ingredient.getAttribute("data-send-ingredients-id-value")))
-    const url = "http://localhost:3000/pantry/pantry_ingredients";
+    const url = this.urlValue;
+    console.log(url);
     fetch(url, {
       method: "POST",
       headers: {  "Accept": "text/plain", 'Content-Type': 'application/json' } ,
