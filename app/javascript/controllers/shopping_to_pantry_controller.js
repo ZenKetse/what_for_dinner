277e3c1d0e40ff6ed7d4da3f33579b4ae88a2ed5
afonso miguel <afonso.miguel1995@gmail.com>
2022-03-09 09:46:28 +0000

    shopping list now displays correct messages

--- app/javascript/controllers/shopping_to_pantry_controller.js
+++ app/javascript/controllers/shopping_to_pantry_controller.js
@@ -20,6 +20,7 @@ export default class extends Controller {
     })
     document.querySelectorAll(".card-ingredient").forEach((ingredient) => ingredient.remove());
     document.querySelectorAll("p").forEach((p) => p.remove())
+    document.querySelector(".message").innerHTML = "<h1>Your shopping list 🛒</h1><h6 data-shopping-to-pantry-target='message' style='font-size: 14px; color: grey; margin-bottom: 20px; margin-left: 3px;'>You have everything in stock!</p>"
   }
 
 }
