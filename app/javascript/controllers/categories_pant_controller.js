0ea6a0bb788a341fc01c9e9fe4aad8c73f674ce4
Francisca Abecasis <francisca.correa1998@gmail.com>
2022-03-09 15:24:55 +0000

    category styling done (I guess) + bugs fixed

---
+++ app/javascript/controllers/categories_pant_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const category = urlParams.get('category')
+
+    if (category) {
+      document.getElementById(category).classList.add("selected-category-pantry")
+    }
+  }
+}
