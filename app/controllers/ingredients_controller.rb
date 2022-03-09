dd79c411eb6a79a6434c8306e6c14c72fef20d2a
afonso miguel <afonso.miguel1995@gmail.com>
2022-03-09 11:44:01 +0000

    category buttons working

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,6 +2,16 @@ class IngredientsController < ApplicationController
   def index
     if params[:query].present?
       @all_ingredients = Ingredient.search_by_name(params[:query])
+    elsif params[:category].present?
+      @all_ingredients = []
+      set_category.each do |categories|
+        categories.each do |category|
+          Ingredient.all.where(category: category).each do |ingredient|
+            @all_ingredients << ingredient
+          end
+        end
+      end
+      @all_ingredients = @all_ingredients.uniq
     else
       @all_ingredients = Ingredient.all
     end
@@ -12,6 +22,14 @@ class IngredientsController < ApplicationController
     respond_to do |format|
       format.html
       format.text { render partial: "ingredients/ingredient_card", locals: { ingredient: @ingredient }, formats: [:html] }
+    end
+  end
+
+  private
+
+  def set_category
+    @categories = params[:category].split.map do |category|
+      Category.search_by_category(category)
     end
   end
 end
